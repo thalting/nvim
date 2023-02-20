63e333fb06cf10a3c73eabc1b64ad13bf476452a
halting <halting@riseup.net>
2023-02-20 12:18:20 -0300

    lsp
    remove decorators in clangd
    border = double
    sumneko_lua -> lua_ls

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,8 @@
 local lspconfig = require("lspconfig")
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local servers = {
     "rust_analyzer",
-    "sumneko_lua",
+    "lua_ls",
     "clojure_lsp",
     "ocamllsp",
     "pyright",
@@ -14,18 +13,32 @@ local servers = {
     "zls",
 }
 
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- LSP settings (for overriding per client)
 local handlers = {
     ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "double" }),
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "double" }),
 }
+vim.diagnostic.config({
+    float = { border = "double" },
+})
 
 for _, lsp in ipairs(servers) do
-    if lsp == "sumneko_lua" then
-        lspconfig.sumneko_lua.setup({
+    if lsp == "clangd" then
+        lspconfig.clangd.setup({
+            cmd = { "clangd", "--header-insertion-decorators=false" },
+            capabilities = capabilities,
+            handlers = handlers,
+        })
+    elseif lsp == "lua_ls" then
+        lspconfig.lua_ls.setup({
             settings = {
                 Lua = {
+                    format = {
+                        enable = false,
+                    },
                     runtime = {
                         version = "LuaJIT",
                     },

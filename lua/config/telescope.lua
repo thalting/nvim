0563f0737801014c3c65917d04a0abb126ab3d7c
halting <halting@riseup.net>
2023-02-22 09:15:10 -0300

    telescope config

---
+++ lua/config/telescope.lua
@@ -0,0 +1,17 @@
+require("telescope").setup({
+    extensions = {
+        undo = {
+            mappings = {
+                ["n"] = {
+                    ["ya"] = require("telescope-undo.actions").yank_additions,
+                    ["yd"] = require("telescope-undo.actions").yank_deletions,
+                    ["<CR>"] = require("telescope-undo.actions").restore,
+                },
+                ["i"] = {
+                    ["<CR>"] = require("telescope-undo.actions").restore,
+                },
+            },
+        },
+    },
+})
+require("telescope").load_extension("undo")

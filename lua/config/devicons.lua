3549fc4ce4a7d7150f668c3c277722f08cb0cf10
halting <halting@riseup.net>
2023-02-23 19:02:15 -0300

    add devicons config file

---
+++ lua/config/devicons.lua
@@ -0,0 +1,46 @@
+return {
+    override = {
+        scm = {
+            icon = "λ",
+            color = "#e37933",
+            cterm_color = "173",
+            name = "Scheme",
+        },
+        fnl = {
+            color = "#fff3d7",
+            icon = "◐",
+            cterm_color = "230",
+            name = "Fennel",
+        },
+        GNUmakefile = {
+            icon = "",
+            color = "#6d8086",
+            cterm_color = "66",
+            name = "Makefile",
+        },
+        mk = {
+            icon = "",
+            color = "#6d8086",
+            cterm_color = "66",
+            name = "Makefile",
+        },
+        Makefile = {
+            icon = "",
+            color = "#6d8086",
+            cterm_color = "66",
+            name = "Makefile",
+        },
+        makefile = {
+            icon = "",
+            color = "#6d8086",
+            cterm_color = "66",
+            name = "Makefile",
+        },
+        default_icon = {
+            icon = "",
+            color = "#6d8086",
+            cterm_color = "66",
+            name = "Default",
+        },
+    },
+}

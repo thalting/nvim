5b01bf43c7e5bdb802ba65fa51194551c03b2162
halting <halting@riseup.net>
2023-06-03 01:08:47 -0300

    add autocmds.lua

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("opts")
 require("plugins")
 require("keybinds")
+require("autocmds")
---
+++ lua/autocmds.lua
@@ -0,0 +1,42 @@
+-- don't auto commenting new lines
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        vim.opt.formatoptions:remove({ "c", "o", "r" })
+    end,
+})
+
+-- highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- show cursor line only in active window
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    callback = function()
+        local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
+        if ok and cl then
+            vim.wo.cursorline = true
+            vim.api.nvim_win_del_var(0, "auto-cursorline")
+        end
+    end,
+})
+vim.api.nvim_create_autocmd({ "InsertEnter", "WinLeave" }, {
+    callback = function()
+        local cl = vim.wo.cursorline
+        if cl then
+            vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
+            vim.wo.cursorline = false
+        end
+    end,
+})
+
+-- disable semantic highlight
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+            vim.api.nvim_set_hl(0, group, {})
+        end
+    end,
+})
